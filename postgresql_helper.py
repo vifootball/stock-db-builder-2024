d8ac3d4e39e1adf360359bce977cd6e791ae1a43
Chung Dongwook <chungdongwook@MacBook-Pro.local>
2024-09-17 00:12:56 +0900

    wip

--- postgresql_helper.py
+++ postgresql_helper.py
@@ -59,9 +59,6 @@ def create_database(database_name):
 
 def copy_csv_files_to_db(csv_dir_path, table_name, create_table_query,):
     connection = get_db_connection()
-    # table_name = "DW_L1_HISTORY"
-    # csv_dir_path = os.path.join("downloads", "history", "chunks")
-
     try:
         with connection: # connection 블록 내에서 모든 쿼리가 실행되므로, 모든 명령어가 성공하면 connection.commit()이 자동으로 호출
             with connection.cursor() as cursor:
@@ -108,14 +105,57 @@ def copy_csv_files_to_db(csv_dir_path, table_name, create_table_query,):
         connection.close()
 
 
+def copy_csv_file_to_db(csv_file_path, table_name, create_table_query):
+    connection = get_db_connection()
+    try:
+        with connection: # connection 블록 내에서 모든 쿼리가 실행되므로, 모든 명령어가 성공하면 connection.commit()이 자동으로 호출
+            with connection.cursor() as cursor:
+
+                # 기존 테이블 삭제
+                print("Drop Table if exists: Started")
+                cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
+                print("Drop Table if exists: Ended")
+                print()
+
+                # 테이블 생성 - CSV 파일에 맞게 컬럼 타입 설정
+                print(f'Create Table "{table_name}": Started')
+                cursor.execute(create_table_query)
+                print(f'Create Table "{table_name}": Ended')
+                print()
+
+                print(f"Upload data from {csv_file_path} to the table '{table_name}': Started")
+
+                # COPY 명령어를 사용하여 CSV 파일을 통째로 테이블에 업로드
+                with open(csv_file_path, 'r') as f:
+                    cursor.copy_expert(f"COPY {table_name} FROM STDIN WITH CSV HEADER DELIMITER ','", f)
+
+                print(f"Upload data from {csv_file_path} to the table '{table_name}': Ended")
+
+                # 업로드된 행 수를 데이터베이스에서 직접 확인
+                cursor.execute(f"SELECT COUNT(*) FROM {table_name};")
+                row_count = cursor.fetchone()[0]
+                print(f"Current total rows in '{table_name}': {row_count}")
+                print()
+
+    except Exception as e:
+        # 예외 발생 시 롤백
+        print(f"An error occurred: {e}")
+        connection.rollback()
+
+    finally:
+        # 연결 닫기
+        connection.close()    
+
+
 if __name__ == "__main__":
     create_database(database_name="stock_db_2024")
     print()
 
     # COPY FILES TO DB: DW_L1_HISTORY
-    copy_csv_files_to_db(
-        csv_dir_path = os.path.join("downloads", "history", "chunks"),
-        table_name = "DW_L1_HISTORY",
-        create_table_query = CREATE_TABLE_DW_L1_HISTORY
-    )
+    # copy_csv_files_to_db(
+    #     csv_dir_path = os.path.join("downloads", "history", "chunks"),
+    #     table_name = "DW_L1_HISTORY",
+    #     create_table_query = CREATE_TABLE_DW_L1_HISTORY
+    # )
     
+    # COPY FILE TO DB: DW_L0_SYMBOL_MASTER
