707d4dba441585e653054eecd71d9a025b1b389a
Chung Dongwook <chungdongwook@MacBook-Pro.local>
2024-09-18 22:17:49 +0900

    refactor

--- collect_history.py
+++ collect_history.py
@@ -10,38 +10,6 @@ from tqdm import tqdm
 
 warnings.filterwarnings('ignore', category=FutureWarning)
 
-
-# LOAD SYMBOLS
-
-# LOAD SYMBOLS - ETF
-etf_master = pd.read_csv(os.path.join('downloads', 'master', 'etf_master.csv'))
-etf_symbols = etf_master['symbol'].to_list()
-etf_symbols = [x for x in etf_symbols if x not in Etfs.EXCLUDE][:100]
-enumerated_etf_symbols = list(enumerate(etf_symbols)) # ray에러 방지용으로 반복문마다 실행순서를 부여하고 이에 따라 약간의 delay를 주는데, 그 순서를 enumerate로 가져옴
-
-# LOAD SYMBOLS - CURRENCY
-currency_master = pd.read_csv(os.path.join('downloads', 'master', 'currency_master.csv'))
-currency_symbols = currency_master['symbol'].to_list()
-enumerated_currency_symbols = list(enumerate(currency_symbols))
-
-# LOAD SYMBOLS - INDEX FOR YAHOO
-indices_master_yahoo = pd.read_csv(os.path.join('downloads', 'index_master', 'index_master_yahoo.csv'))
-indices_symbols_yahoo = indices_master_yahoo['symbol'].to_list()
-
-indices_master_investpy = pd.read_csv(os.path.join('downloads', 'index_master', 'index_master_investpy.csv'))
-indices_symbols_investpy = indices_master_investpy['symbol'].to_list()
-
-indices_master_fd = pd.read_csv(os.path.join('downloads', 'index_master', 'index_master_fd.csv'))
-indices_symbols_fd = indices_master_fd['symbol'].to_list()
-
-indices_symbols_general = list(set(indices_symbols_yahoo + indices_symbols_investpy + indices_symbols_fd))
-enumerated_indices_symbols_general = list(enumerate(indices_symbols_general))
-
-# LOAD SYMBOLS - INDEX FOR FRED
-indices_master_fred = pd.read_csv(os.path.join('downloads', 'index_master', 'index_master_fred.csv'))
-indices_symbols_fred = list(enumerate(indices_master_fred['symbol'].to_list()))
-
-
 #### USING RAY
 @ray.remote
 def collect_history(order, symbol, method='yf', put_dirpath='./downloads/history/etf'):
